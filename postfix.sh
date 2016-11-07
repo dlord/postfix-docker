bd2229b770704b8414f14946fe0c3b8070d040d7
John Paul Alcala <ore.wa.dragon.lord@gmail.com>
2016-11-07 17:05:07 +0800

    Improvements to startup and shutdown routines
    Dovecot is now back to running as a background process.
    We now tail the syslog. Also, the main syslog is reset at every
    restart.
    
    To ensure that the container is cleanly shutdown, we ensure that
    the tail process is killed nicely.

--- postfix.sh
+++ postfix.sh
@@ -179,6 +179,10 @@ function start_all() {
     # ensure that postfix and crond pid file has been removed.
     rm -f /var/spool/postfix/pid/master.pid /var/run/crond.pid
 
+    # reset syslog
+    cat /dev/null > /var/log/syslog
+    chown syslog:adm /var/log/syslog
+
     cron
     service rsyslog start
     service opendkim start
@@ -188,11 +192,16 @@ function start_all() {
     service clamav-milter start
     service clamav-freshclam start
     service postfix start
-    dovecot -F
+    dovecot
+
+    tail -n 1000 -f /var/log/syslog &
+    TAIL_PID=$!
+    wait $TAIL_PID
 }
 
 function stop_all() {
     echo "Shutting down..."
+    dovecot stop
     service postfix stop
     service clamav-freshclam stop
     service clamav-milter stop
@@ -202,6 +211,7 @@ function stop_all() {
     service opendkim stop
     service rsyslog stop
     kill `cat /var/run/crond.pid`
+    kill "$TAIL_PID"
 }
 
 trap stop_all EXIT

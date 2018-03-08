e2febcda80ad3642e517d69b6770d01bbd468a94
John Paul Alcala <jp@jpalcala.com>
2018-03-09 00:08:09 +0800

    Added capability to run app via uwsgi
    Signed-off-by: John Paul Alcala <jp@jpalcala.com>

--- admin/admin/settings.py
+++ admin/admin/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#$t3o8ft0ixtvt@j2%ui5tondw%5c_p)-dm)3(ms*!v!^_s(d1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -123,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")

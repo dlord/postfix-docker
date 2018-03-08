b73d7074fddce7dc478bdd97a9c92a98d74911d5
John Paul Alcala <jp@jpalcala.com>
2018-03-09 00:17:17 +0800

    Implemented per-user restrictions to the postfix models.
    Staff users in Django admin that don't have super user privileges
    can now be granted per-domain management capabilities. Among them
    are:
    
    1. Add/Edit email users per domain
    2. Define aliases per domain
    
    Also, this enforces max_accounts per domain, and can only be
    increased by editing the domain.
    
    Signed-off-by: John Paul Alcala <jp@jpalcala.com>

--- admin/postfix/admin.py
+++ admin/postfix/admin.py
@@ -1,26 +1,76 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 from django.forms import ModelForm, CharField, PasswordInput
+from django import forms
 
-from .models import Domain, EmailUser
+from .models import Domain, EmailUser, Alias
 
 import os, crypt, hashlib
 
 class EmailUserForm(ModelForm):
+    field_order = ['domain', 'username', 'password2', 'full_name', 'active',]
     password2 = CharField(widget=PasswordInput, label="Password", required=False)
-    
+
+    def clean(self):
+        cleaned_data = super(EmailUserForm, self).clean()
+
+        if not self.instance.id:
+            domain = cleaned_data.get('domain')
+            if domain and EmailUser.objects.filter(domain=domain).count() >= domain.max_accounts:
+                raise forms.ValidationError('Reached the max number of accounts for this domain.')
+
     class Meta:
         model = EmailUser
         fields = ['domain', 'username', 'password2', 'full_name', 'active',]
 
+class AliasForm(ModelForm):
+    field_order = ['domain', 'source', 'destination', 'active',]
+
+    class Meta:
+        model = Alias
+        fields = ['domain', 'source', 'destination', 'active',]
+
+class DomainInline(admin.TabularInline):
+    model = Domain.admins.through
+    can_delete = False
+    verbose_name = 'Domain'
+    verbose_name_plural = 'Domains'
+
+class DomainAwareModelAdmin(admin.ModelAdmin):
+    def get_queryset(self, request):
+        queryset = super(DomainAwareModelAdmin, self).get_queryset(request)
+        if request.user.is_superuser:
+            return queryset
+        return queryset.filter(domain__admins=request.user)
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "domain" and not request.user.is_superuser:
+            kwargs["queryset"] = Domain.objects.filter(admins=request.user)
+
+        return super(DomainAwareModelAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (DomainInline, )
+    exclude = ('admins', )
 
 class DomainAdmin(admin.ModelAdmin):
     list_display = ('name', 'active',)
     ordering = ['name',]
 
-class EmailUserAdmin(admin.ModelAdmin):
+class EmailUserAdmin(DomainAwareModelAdmin):
     form = EmailUserForm
-    list_display = ('domain', 'username', 'full_name', 'active',)
-    ordering = ['domain', 'username', 'full_name',]
+    fields = ('domain', 'username', 'full_name', 'password2', 'active', )
+    list_display = ('address', 'active',)
+    list_editable = ('active',)
+    ordering = ['domain', 'full_name', 'username',]
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return self.readonly_fields + ('domain', 'username',)
+
+        return self.readonly_fields
 
     def save_model(self, request, obj, form, change):
         password2 = form['password2'].value()
@@ -31,5 +81,23 @@ class EmailUserAdmin(admin.ModelAdmin):
 
         obj.save()
 
+class AliasAdmin(DomainAwareModelAdmin):
+    form = AliasForm
+    fields = ['domain', 'source', 'destination', 'active',]
+    list_display = ('mapping', 'active',)
+    list_editable = ('active',)
+    ordering = ['domain', 'source',]
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return self.readonly_fields + ('domain',)
+
+        return self.readonly_fields
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
 admin.site.register(Domain, DomainAdmin)
 admin.site.register(EmailUser, EmailUserAdmin)
+admin.site.register(Alias, AliasAdmin)

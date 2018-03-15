0f579615faf7d448297f6e54b5adec37db31304c
John Paul Alcala <jp@jpalcala.com>
2018-03-15 10:29:49 +0800

    Revert "Spam learning is now executed by debian-spamd"
    Apparently, the debian-spamd user cannot access the mail folders.
    And I'd rather not fix the permissions of that folder, since postfix creates
    files with a 600 permission by default.
    
    This reverts commit 6c90139c22b75100e757420df66076d118f81232.

--- learnspam.sh
+++ learnspam.sh
@@ -3,9 +3,11 @@
 echo "Analyzing ham messages..."
 for i in /var/mail/vmail/*/*/mail/{cur,new}/; do
     sa-learn --ham $i
+    chown -R debian-spamd:debian-spamd /var/lib/spamassassin
 done
 
 echo "Analyzing spam messages..."
 for i in /var/mail/vmail/*/*/mail/Junk/; do
     sa-learn --spam $i
+    chown -R debian-spamd:debian-spamd /var/lib/spamassassin
 done

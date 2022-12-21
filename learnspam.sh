0547f3b33702c4fb9a6101cf746a26bc09734b57
John Paul Alcala <dragonlord@Illustra.local>
2022-12-21 19:48:52 +0800

    rspamc output will now be written to log files.
    This makes it easier to inspect if there is something wrong
    with the process.

--- learnspam.sh
+++ learnspam.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
+echo > /var/log/rspamc_ham.log
+echo > /var/log/rspamc_spam.log
+
 echo "Analyzing ham messages..."
 for i in /var/mail/vmail/*/*/mail/{cur,new}/; do
-    rspamc learn_ham $i
+    rspamc learn_ham $i >> /var/log/rspamc_ham.log 2>&1
     chown -R _rspamd:_rspamd /var/lib/rspamd
 done
 
 for i in /var/mail/vmail/*/*/mail/Archive/{cur,new}/; do
-    rspamc learn_ham $i
+    rspamc learn_ham $i >> /var/log/rspamc_ham.log 2>&1
     chown -R _rspamd:_rspamd /var/lib/rspamd
 done
 
 
 echo "Analyzing spam messages..."
 for i in /var/mail/vmail/*/*/mail/Junk/{cur,new}/; do
-    rspamc learn_spam $i
+    rspamc learn_spam $i >> /var/log/rspamc_spam.log 2>&1
     chown -R _rspamd:_rspamd /var/lib/rspamd
 done
